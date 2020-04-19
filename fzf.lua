3e0b08ac19be817495c3cf7d76ca4c2db42bcc3e
2pac <tarasyarema@pm.me>
2020-04-19 19:35:23 +0200

    added ff command

--- fzf.lua
+++ fzf.lua
@@ -14,8 +14,44 @@ function fzf(bp, opt)
 	end
 end
 
+function fzf_find(bp, args, opt)
+	local strings = import("strings")
+	local cmd = "grep --line-buffered -rnwi "
+
+	local pattern = ""
+
+	for i = 1, #args do
+		pattern = args[i] .. " "
+	end
+
+	cmd = cmd .. pattern .. " * | fzf"
+	
+	local output, err = shell.RunInteractiveShell(cmd, false, true)
+	-- local run, err = shell.RunBackgroundShell(cmd)
+	-- local output = run()
+	
+	if err ~= nil then
+	    micro.InfoBar():Error("could not execute fzf command: ", err)
+	    return
+   	end
+
+	-- output = strings.Split(output, "\n")
+	-- micro:TermMessage("\nOutput\n", output)
+	
+	local input = strings.Split(output, ":")
+
+	if #input < 2 then
+	    micro.InfoBar():Error("bad input format: ", input)
+	    return
+   	end
+   	
+	fzfOpenLine(input[1], bp, 2, input[2])
+	micro.InfoBar():Message(input[1] .. " @ " .. input[2])
+end
+
 function fzfOpen(output, bp, opt)
     local strings = import("strings") 
+    local strconv = import("strconv") 
     file = strings.TrimSpace(output)
 
 	if file ~= "" then
@@ -31,7 +67,34 @@ function fzfOpen(output, bp, opt)
 			bp:HSplitBuf(buf)
 		elseif opt == 2 then
 			bp:VSplitBuf(buf)
-		end		
+		end
+	end
+end
+
+function fzfOpenLine(output, bp, opt, line)
+    local strings = import("strings") 
+    local strconv = import("strconv") 
+    file = strings.TrimSpace(output)
+
+	if file ~= "" then
+		local buf, err = buffer.NewBufferFromFile(file)
+
+		if err ~= nil then
+			micro.InfoBar():Error(err)
+		end
+
+		local new_bp
+
+		if opt == 0 then
+			new_bp = bp:OpenBuffer(buf)
+		elseif opt == 1 then
+			new_bp = bp:HSplitBuf(buf)
+		elseif opt == 2 then
+			new_bp =bp:VSplitBuf(buf)
+		end
+
+		new_bp.Cursor.Y = tonumber(line)-1
+		new_bp.Cursor:Relocate()
 	end
 end
 
@@ -47,6 +110,10 @@ function init()
     config.MakeCommand("fv", function(bp, args)
 		fzf(bp, 2)
     end, config.NoComplete)
-    
+
+    config.MakeCommand("ff", function(bp, args)
+		fzf_find(bp, args, 2)
+    end, config.NoComplete)
+
     config.AddRuntimeFile("fzf", config.RTHelp, "help/fzf.md")
 end
